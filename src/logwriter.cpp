3b7bc761d890e5f665bcddd6e6a37b59527af12b
idfumg <idfumg@gmail.com>
2018-05-07 13:17:29 +0300

    refact

--- src/logwriter.cpp
+++ src/logwriter.cpp
@@ -13,188 +13,194 @@
 
 #include <string>
 
+#include "return.h"
+
+using namespace std;
+
 constexpr char filename[] = "log";
 
-static void log(const std::string& data)
+static int log(FILE* file, const std::string& data)
 {
-    const auto file = fopen(filename, "aw");
-    if (file == NULL) {
-      perror("fopen");
-      exit(2);
+    if (const auto ret = fputs((data + "\n").c_str(), file) < 0) {
+        return ret;
     }
 
-    if (setvbuf(file, NULL, _IONBF, 0) < 0) {
-      perror("setvbuf");
-      exit(3);
+    return 0;
+}
+
+static int create_socket()
+{
+    const auto fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0) {
+        return fd;
     }
 
-    if (fputs((data + "\n").c_str(), file) < 0) {
-      perror("fputs");
-      exit(4);
+    unlink("./logsocket");
+
+    struct sockaddr_un address;
+    memset(&address, 0, sizeof(address));
+    address.sun_family = AF_UNIX;
+    snprintf(address.sun_path, PATH_MAX, "./logsocket");
+
+    if (const auto ret = bind(fd, (struct sockaddr*) &address, sizeof(address)) < 0) {
+        return ret;
     }
 
-    if (fclose(file) < 0) {
-      perror("fclose");
-      exit(5);
+    if (const auto ret = listen(fd, 256) < 0) {
+        return ret;
     }
+
+    int on = 1;
+    if (const auto ret = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*) &on, sizeof(on)) < 0) {
+        return ret;
+    }
+
+    on = 1;
+    if (const auto ret = ioctl(fd, FIONBIO, (char*) &on) < 0) {
+        return ret;
+    }
+
+    return fd;
+}
+
+static void compress_array(struct pollfd fds[], int& nfds)
+{
+    for (auto i = 0; i < nfds; ++i) {
+        if (fds[i].fd == -1) {
+            for (auto j = i; j < nfds; ++j) {
+                fds[j].fd = fds[j+1].fd;
+            }
+            --nfds;
+        }
+    }
+}
+
+static int polling_socket(const int fd, FILE* file)
+{
+    struct pollfd fds[256];
+    memset(fds, 0, sizeof(fds));
+    fds[0].fd = fd;
+    fds[0].events = POLLIN;
+
+    int nfds = 1;
+    bool is_compress_array = false;
+
+    for (;;) {
+        const auto rc = poll(fds, nfds, -1);
+
+        if (rc < 0) {
+            return rc;
+        }
+
+        if (rc == 0) {
+            continue;
+        }
+
+        const auto current_size = nfds;
+        for (auto i = 0; i < current_size; ++i) {
+            if (fds[i].revents == 0) {
+                continue;
+            }
+
+            if (fds[i].revents != POLLIN) {
+                continue;
+            }
+
+            if (fds[i].fd == fd) {
+                for (;;) {
+                    const auto new_fd = accept(fd, NULL, NULL);
+                    if (new_fd < 0) {
+                        if (errno != EWOULDBLOCK) {
+                            return new_fd;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+
+                    fds[nfds].fd = new_fd;
+                    fds[nfds].events = POLLIN;
+                    ++nfds;
+                }
+            }
+            else {
+                for (;;) {
+                    std::string data;
+
+                    char buffer[BUFSIZ] = {0};
+                    const auto rc = recv(fds[i].fd, buffer, sizeof(buffer) -1, 0);
+
+                    if (rc < 0) {
+                        if (errno != EWOULDBLOCK) {
+                            return rc;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+
+                    if (rc == 0) {
+                        close(fds[i].fd);
+                        fds[i].fd = -1;
+                        is_compress_array = true;
+                        break;
+                    }
+
+                    data = buffer;
+
+                    if (not data.empty()) {
+                        if (const auto ret = log(file, data) < 0) {
+                            return ret;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (is_compress_array) {
+            compress_array(fds, nfds);
+            is_compress_array = false;
+        }
+    }
+
+    for (int i = 0; i < nfds; ++i) {
+        if (fds[i].fd >= 0) {
+            close(fds[i].fd);
+        }
+    }
+
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-  log(argv[0] + std::string(" started"));
-
-  const auto fd = socket(PF_UNIX, SOCK_STREAM, 0);
-  if (fd < 0) {
-    perror("socket");
-    exit(6);
-  }
-
-  unlink("./logsocket");
-
-  struct sockaddr_un address;
-  memset(&address, 0, sizeof(address));
-  address.sun_family = AF_UNIX;
-  snprintf(address.sun_path, PATH_MAX, "./logsocket");
-
-  const auto bind_ret = bind(fd,
-                             (struct sockaddr*) &address,
-                             sizeof(address));
-  if (bind_ret < 0) {
-    perror("bind");
-    exit(7);
-  }
-
-  const auto listen_ret = listen(fd, 256);
-  if (listen_ret < 0) {
-    perror("listen");
-    exit(8);
-  }
-
-  int on = 1;
-  const auto setsockopt_ret = setsockopt(fd,
-                                         SOL_SOCKET,
-                                         SO_REUSEADDR,
-                                         (char*) &on,
-                                         sizeof(on));
-  if (setsockopt_ret < 0) {
-    perror("setsockopt");
-    exit(9);
-  }
-
-  on = 1;
-  const auto ioctl_ret = ioctl(fd, FIONBIO, (char*) &on);
-  if (ioctl_ret < 0) {
-    perror("ioctl");
-    exit(10);
-  }
-
-  struct pollfd fds[256];
-  memset(fds, 0, sizeof(fds));
-  fds[0].fd = fd;
-  fds[0].events = POLLIN;
-
-  int nfds = 1;
-  bool compress_array = false;
-
-  for (;;) {
-    const auto rc = poll(fds, nfds, -1);
-
-    if (rc < 0) {
-      perror("poll");
-      exit(11);
+    const auto file = fopen(filename, "aw");
+    if (file == NULL) {
+        return -1;
     }
 
-    if (rc == 0) {
-      continue;
+    if (const auto ret = setvbuf(file, NULL, _IONBF, 0) < 0) {
+        return ret;
     }
 
-    const int current_size = nfds;
-    for (int i = 0; i < current_size; ++i) {
-      if (fds[i].revents == 0) {
-        continue;
-      }
-
-      if (fds[i].revents != POLLIN) {
-        continue;
-      }
-
-      if (fds[i].fd == fd) {
-        for (;;) {
-          printf("new connection accepted\n");
-          const auto new_fd = accept(fd, NULL, NULL);
-          if (new_fd < 0) {
-            if (errno != EWOULDBLOCK) {
-              perror("accept");
-              exit(12);
-            }
-            else {
-              break;
-            }
-          }
-
-          fds[nfds].fd = new_fd;
-          fds[nfds].events = POLLIN;
-          ++nfds;
-        }
-      }
-      else {
-        printf("some data received\n");
-
-
-        for (;;) {
-          printf("read data from socket\n");
-          std::string data;
-
-          char buffer[BUFSIZ] = {0};
-          const auto rc = recv(fds[i].fd, buffer, sizeof(buffer) -1, 0);
-
-          if (rc < 0) {
-            if (errno != EWOULDBLOCK) {
-              perror("recv");
-              exit(13);
-            }
-            else {
-              break;
-            }
-          }
-
-          if (rc == 0) {
-            close(fds[i].fd);
-            fds[i].fd = -1;
-            compress_array = true;
-            break;
-          }
-
-          data = buffer;
-
-          if (not data.empty()) {
-            printf("new data received: %s\n", data.c_str());
-            log(data);
-          }
-        }
-      }
+    if (const auto ret = log(file, argv[0] + std::string(" started")) < 0) {
+        return ret;
     }
 
-    if (compress_array) {
-      for (int i = 0; i < nfds; ++i) {
-        if (fds[i].fd == -1) {
-          for (int j = i; j < nfds; ++j) {
-            fds[j].fd = fds[j+1].fd;
-          }
-          --nfds;
-        }
-      }
-      compress_array = false;
+    const auto socket = create_socket();
+    if (socket < 0) {
+        return socket;
     }
-  }
 
-  for (int i = 0; i < nfds; ++i) {
-    if (fds[i].fd >= 0) {
-      close(fds[i].fd);
+    if (const auto ret = polling_socket(socket, file)) {
+        return ret;
     }
-  }
 
-  close(fd);
+    close(socket);
 
-  return 0;
+    if (const auto ret = fclose(file) < 0) {
+        return ret;
+    }
+
+    return 0;
 }
